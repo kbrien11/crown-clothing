8adc0d64644528517d947bcc5b05eb9fb5b81692
Keith <kbrien11@gmail.com>
2020-10-22 12:05:55 -0400

    updated menu items to navitage with router

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -11,29 +11,34 @@ class Directory extends React.Component {
             sections:[{
                 title: 'hats',
                 imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-                id:1
+                id:1,
+                linkurl:'hats'
             },
             {
                 title: 'jackets',
                 imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-                id:2
+                id:2,
+                linkurl:''
             },
             {
                 title: 'sneakers',
                 imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-                id:3
+                id:3,
+                linkurl:''
             },
             {
                 title: 'womans',
                 imageUrl: "https://i.ibb.co/GCCdy8t/womans.png",
                 size:"large",
-                id:4
+                id:4,
+                linkurl:''
             },
             {
                 title: 'mens',
                 imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
                 size:'large',
-                id:5
+                id:5,
+                linkurl:''
             }
 
         
@@ -44,8 +49,8 @@ class Directory extends React.Component {
     render() {
         return (
             <div className = 'directory-menu'>
-                    {this.state.sections.map( ({title,imageUrl,id,size}) => (
-                        <MenuItem key = {id} title={title} imageUrl = {imageUrl} size = {size} />
+                    {this.state.sections.map( ({title,imageUrl,id,size,linkurl}) => (
+                        <MenuItem key = {id} title={title} imageUrl = {imageUrl} size = {size} linkurl={linkurl} />
                     )
 
                     )}
