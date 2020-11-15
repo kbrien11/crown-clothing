175b063a8a0e4c5fb60a2416f41d8a49c181fe45
Keith <kbrien11@gmail.com>
2020-11-15 14:47:47 -0500

    added sighin

--- src/App.js
+++ src/App.js
@@ -45,18 +45,24 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header/>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route exact path='/checkout' component={CheckoutPage} />
-           {()=>
-             this.props.currentUser? (
-             <Redirect to ='/' />
-             )
-          :<SignInAndSignUpPage />} />
-        </Switch>
-      </div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route exact path='/checkout' component={CheckoutPage} />
+        <Route
+          exact
+          path='/signin'
+          render={() =>
+            this.props.currentUser ? (
+              <Redirect to='/' />
+            ) : (
+              <SignInAndSignUpPage />
+            )
+          }
+        />
+      </Switch>
+    </div>
     );
   }
 }
